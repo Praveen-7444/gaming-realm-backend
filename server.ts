1457baafd4bb4a3c7332bef11dcb25e4c8516ac4
praveen-7444 <praveenn7111@gmail.com>
2025-03-19 13:50:33 +0530

    done user authentication

--- server.ts
+++ server.ts
@@ -1,18 +1,30 @@
 import Fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
 
 const fastify = Fastify();
-const prisma = new PrismaClient();
 
-fastify.get('/users', async (request, reply) => {
-  const users = await prisma.user.findMany();
-  reply.send(users);
+
+import fastifyCookie from "@fastify/cookie";
+
+fastify.register(fastifyCookie, {
+  secret: process.env.COOKIE_SECRET, 
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+
+import userRoutes from './src/routes/user.route';
+import authRoutes from './src/routes/auth.route';
+
+fastify.register(userRoutes, { prefix: '/api/users'});
+fastify.register(authRoutes, { prefix: '/api/auth'});
+
+
+
+
+fastify.listen({ port: 8000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server running at ${address}`);
+  console.log(`Server running at http://localhost:${process.env.PORT || 8000}`);
 });
+
+
---
+++ src/controller/schemas/user.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+  name: z.string().min(2).max(255),
+  email: z.string().email(),
+  username: z.string().min(2).max(255),
+  password: z.string().min(6).max(255),
+});
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
